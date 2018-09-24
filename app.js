17607e5df76abae5d84db532e36fc0132d478a21
Ben Harned <bendesigns76@gmail.com>
2018-09-25 01:40:32 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//This means when the page is ready load this function
+$(document).ready(function(){
+	
+var quote
+var author
+	function getNewQuote() {
+		$.ajax({
+			//You can add all this info in the url also.(same result)
+      // url: 'https://talaikis.com/api/quotes/random/ ',
+      url: "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en",
+			json: 'json',
+			dataType: 'json',
+			data: {
+				method: 'getQuote',
+				lang: 'en',
+				format: 'json'				
+			},
+			success: function(response) {
+				quote = response.quoteText;
+				author = response.quoteAuthor;
+				$('#quote').text(quote);
+				if (author) {
+					$('#author').text('- ' + author + ' -');
+				} else {
+					$('#author').text('-Unknown-');
+				}
+			}
+		});
+	}
+	getNewQuote();
+
+	$('#quote-button').on('click', function() {
+		getNewQuote();
+	});
+	$('#twit').on('click', function(event) {
+		window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote + ' -' + author + '-'));
+	});
+});
+
+
+
+
+	
