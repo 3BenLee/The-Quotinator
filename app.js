1dc212d56f0623008b29ef0e776e05f7f83beb34
Ben Harned <bendesigns76@gmail.com>
2018-09-26 12:02:17 +0900

    Fixed ajazx call.

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var author
 		$.ajax({
 			//You can add all this info in the url also.(same result)
       // url: 'https://talaikis.com/api/quotes/random/ ',
-      url: "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en",
-			json: 'json',
-			dataType: 'json',
+      url: "http://api.forismatic.com/api/1.0/",
+			jsonp: 'jsonp',
+			dataType: 'jsonp',
 			data: {
 				method: 'getQuote',
 				lang: 'en',
-				format: 'json'				
+				format: 'jsonp'				
 			},
 			success: function(response) {
 				quote = response.quoteText;
